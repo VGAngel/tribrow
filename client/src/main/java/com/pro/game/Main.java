e1585230c5c4321f571d2d489c1ae5614a000610
ValentynPolishchuk <V.POLISHCHUK@external.oberthur.com>
2014-08-13 18:46:09 +0300

    lua

---
+++ client/src/main/java/com/pro/game/Main.java
@@ -0,0 +1,13 @@
+package com.pro.game;
+
+import com.jme3.app.SimpleApplication;
+import com.pro.gui.HelloNiftySelectApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleApplication app = new HelloNiftySelectApp();
+        app.start();
+    }
+
+}
