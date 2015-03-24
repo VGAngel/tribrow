c02090ed81adf0085d560d5b38c5ae3dbd149415
ValentynPolishchuk <Valentyn.Polishchuk@infopulse.com.ua>
2015-03-24 21:45:44 +0200

    lua1

---
+++ model/src/main/java/com/pro/game/model/Animal.java
@@ -0,0 +1,17 @@
+package com.pro.game.model;
+
+/**
+ * Created by admin on 10.03.2015
+ */
+public class Animal {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
